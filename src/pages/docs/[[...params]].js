27fe70403bb1521e2354c57bac1b5d1da257dd9c
radenmasabdul <abdul.tamsis@gmail.com>
2023-03-18 03:19:30 +0700

    Catch All Routes

---
+++ src/pages/docs/[[...params]].js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router"
+
+const Doc = () => {
+    const router = useRouter();
+    const { params = [] } = router.query;
+    // console.log(params);
+
+    if (params.length === 2) {
+        return (
+            <h1 className="text-red-700 text-4xl">
+                Viewing docs for feature {params[0]} and concept {params[1]}
+            </h1>
+        )
+    } else if (params.length === 1) {
+        return <h1 className="text-red-700 text-4xl">
+            Viewing docs for feature {params[0]}
+        </h1>
+    }
+
+    return (
+        <>
+            <h1 className="text-red-700 text-4xl">
+                Documents Home Page
+            </h1>
+        </>
+    )
+}
+
+export default Doc
